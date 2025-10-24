733d21689ff3c8bfc9ff7854a693e3de00323c20
Enggy Puma <empuma1@cps.edu>
2025-10-24 10:18:42 -0500

    finished class dog notes

--- notes.py
+++ notes.py
@@ -12,7 +12,7 @@ class Dog:
         age - the age of the dog
         favorite_food - the dog's favorite food
     """
-    def __init__(self, fur_color, name, age, favorite_food):
+    def __init__(self, fur_color = "black", name = "no name", age = 1, favorite_food = "kibble"):
         """Initialize a new Dog with fur_color, name, age, and favorite_food"""
         self.fur_color = fur_color
         self.name = name
@@ -21,11 +21,23 @@ class Dog:
 
     def __str__(self):
         """return a string representation of a dog"""
-        return f"{self.name} is a {self.age} year old {self.furcolor} dog who loves {self.favoritefood}"
+        return f"{self.name} is a {self.age} year old {self.fur_color} dog who loves {self.favorite_food}"
 
     def bark(self):
         """Make the dog bark"""
         return f"{self.name} says Woof, Woof!!"
+    
+    def birthday(self):
+        """Celebrate the dog's birthday"""
+        self.age += 1
+        print (f"Celebrating {self.name}'s birthday, they are now {self.age} years!")
+
+    def change_favorite_food(self, new_food):
+        """Change the favorite food of the dog"""
+        old_food = self.favorite_food
+        self.favorite_food = new_food
+        print(f"{self.name} used to love {self.favorite_food}")
+
 
 
 
@@ -36,10 +48,17 @@ class Dog:
 
 my_dog = Dog("black", "logan", 9, "salmon")
 enggy_dog = Dog("black and white", "peluchin", 13, "rice")
+default_dog = Dog()
+aaron_dog = Dog("peach and white", "dumbo", favorite_food="anything edible")
 
 print(my_dog)
 print(enggy_dog)
+print(default_dog)
+print(aaron_dog)
     
 print()
     
 print (enggy_dog.bark())
+aaron_dog.birthday()
+
+print (aaron_dog)
